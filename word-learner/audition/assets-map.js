41a7fcadb0bd5b0d1c80530bfc9155e6645fb11e
PavelOfficial <pavelofficial@ya.ru>
2025-02-09 13:33:29 +0300

    8000 words dict

--- word-learner/audition/assets-map.js
+++ word-learner/audition/assets-map.js
@@ -1,4 +1,17 @@
 const auditionUrls = [
   "/audition/texts/0001.json",
   "/audition/texts/0002.json",
+  "/audition/texts/0003.json",
+  "/audition/texts/0004.json",
+  "/audition/texts/0005.json",
+  "/audition/texts/0006.json",
+  "/audition/texts/0007.json",
+  "/audition/texts/0008.json",
+  "/audition/texts/0009.json",
+  "/audition/texts/0010.json",
+  "/audition/texts/0011.json",
+  "/audition/texts/0012.json",
+  "/audition/texts/0013.json",
+  "/audition/texts/0014.json",
+  "/audition/texts/0015.json",
 ]
