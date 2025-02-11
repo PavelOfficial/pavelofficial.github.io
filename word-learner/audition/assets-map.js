82890a68e017ae0ed2b3bf8be96449d19616838a
PavelOfficial <pavelofficial@ya.ru>
2025-02-11 18:43:10 +0300

    8000 words dict

--- word-learner/audition/assets-map.js
+++ word-learner/audition/assets-map.js
@@ -26,6 +26,25 @@ const auditionUrlsA2EnglishCounsel = [
   "/audition/texts/0024.json",
   "/audition/texts/0025.json",
   "/audition/texts/0026.json",
+  "/audition/texts/0027.json",
+  "/audition/texts/0028.json",
+  "/audition/texts/0029.json",
+  "/audition/texts/0030.json",
+]
+
+const auditionUrlsB1EnglishCounsel = [
+  "/audition/texts/0031.json",
+  "/audition/texts/0032.json",
+  "/audition/texts/0033.json",
+  "/audition/texts/0034.json",
+  "/audition/texts/0035.json",
+  "/audition/texts/0036.json",
+  "/audition/texts/0037.json",
+  "/audition/texts/0038.json",
+  "/audition/texts/0039.json",
+  "/audition/texts/0040.json",
+  "/audition/texts/0041.json",
+  "/audition/texts/0042.json",
 ]
 
 const categories = [
@@ -36,5 +55,9 @@ const categories = [
   {
     title: "A2 English Counsel",
     auditions: auditionUrlsA2EnglishCounsel,
+  },
+  {
+    title: "B1 English Counsel",
+    auditions: auditionUrlsB1EnglishCounsel,
   },
 ]
